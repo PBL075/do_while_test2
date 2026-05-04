b9d2bd1dbddd8fb48d28254ba1948863834c8df6
PBL075 <pavelgorea75@gmail.com>
2026-05-04 16:14:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test_while
+{
+    class Program
+    {
+        static void Main()
+        {
+            int incercare = 0;
+
+            do
+            {
+                incercare++;
+                Console.WriteLine("Incercarea : " + incercare);
+            }
+            while (incercare < 0 );
+        }
+    }
+}
